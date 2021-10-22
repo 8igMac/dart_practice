6a8949f6c6520764dc28fe3efbd4531503b510a4
8igmac <8igmac@pm.me>
2021-10-22 17:36:38 +0800

    [FEAT] 'Add' function by Chia-Yung.

---
+++ chia-yung/add.dart
@@ -0,0 +1,3 @@
+int add(int lhs, int rhs) {
+  return lhs + rhs;
+}
---
+++ chia-yung/add_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+import 'add.dart';
+
+void main() {
+  test('Add', () {
+    expect(add(2, 3), 5);
+  });
+}
