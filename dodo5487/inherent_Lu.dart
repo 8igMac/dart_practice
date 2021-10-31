45707c2aad1b442ff8d6fa23f448b37b06172998
dodo5487 <p76101283@gs.ncku.edu.tw>
2021-10-31 15:01:57 +0800

    [feat] inherent_Lu by dodo5487

---
+++ dodo5487/inherent_Lu.dart
@@ -0,0 +1,67 @@
+abstract class Teacher {
+  String name = "";
+  void introduction() {
+    print("My name is $name. I am a teacher , Nice to meet you.");
+  }
+}
+
+class trigger_Lu extends Teacher with nlp {
+  String name = "盧文祥";
+
+  void google() {
+    print("你們不要都迷戀google");
+  }
+
+  void three() {
+    print("實驗室以三為多");
+  }
+
+  void water() {
+    print("我這一輩子都沒有幫我的老師倒水");
+  }
+}
+
+class trigger_Xin extends Teacher with nlp {
+  String name = "張景新";
+
+  void slience() {
+    print("我不知道他講了甚麼");
+  }
+
+  void skill() => print("我會瘋狂換背景");
+}
+
+mixin nlp {
+  void common_skill() {
+    print("我主修NLP");
+  }
+}
+
+class Xin extends trigger_Xin {
+  @override
+  void introduction() {
+    super.introduction();
+    common_skill();
+    skill();
+    slience();
+  }
+}
+
+class Lu extends trigger_Lu {
+  @override
+  void introduction() {
+    super.introduction();
+    common_skill();
+    google();
+    three();
+    water();
+  }
+}
+
+void main() {
+  var L = Lu();
+  var X = Xin();
+
+  L.introduction();
+  X.introduction();
+}
